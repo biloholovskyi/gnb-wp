7c13ca4f13ddcbd773551863a95c10eee2747391
Biloholovskyi <biloholovskyi>
2021-11-10 16:42:08 +0200

    documents

--- wp-content/themes/gnb/functions.php
+++ wp-content/themes/gnb/functions.php
@@ -232,6 +232,45 @@ function register_post_types()
     'rewrite' => true,
     'query_var' => true,
   ));
+
+  register_post_type('documents', array(
+    'label' => null,
+    'labels' => array(
+      'name' => 'Документы', // основное название для типа записи
+      'singular_name' => 'Документ', // название для одной записи этого типа
+      'add_new' => 'Добавить Документ', // для добавления новой записи
+      'add_new_item' => 'Добавление Документа', // заголовка у вновь создаваемой записи в админ-панели.
+      'edit_item' => 'Редактирование Документа', // для редактирования типа записи
+      'new_item' => 'Новый Документ', // текст новой записи
+      'view_item' => 'Смотреть Документ', // для просмотра записи этого типа.
+      'search_items' => 'Искать Документ', // для поиска по этим типам записи
+      'not_found' => 'Не найден Документ', // если в результате поиска ничего не было найдено
+      'not_found_in_trash' => 'Не найден Документ в корзине', // если не было найдено в корзине
+      'parent_item_colon' => '', // для родителей (у древовидных типов)
+      'menu_name' => 'Документы', // название меню
+    ),
+    'description' => '',
+    'public' => true,
+    'publicly_queryable' => true, // зависит от public
+    'exclude_from_search' => true, // зависит от public
+    'show_ui' => true, // зависит от public
+    'show_in_menu' => true, // показывать ли в меню адмнки
+    'show_in_admin_bar' => true, // по умолчанию значение show_in_menu
+    'show_in_nav_menus' => true, // зависит от public
+    'show_in_rest' => null, // добавить в REST API. C WP 4.7
+    'rest_base' => null, // $post_type. C WP 4.7
+    'menu_position' => 4,
+    'menu_icon' => 'dashicons-archive',
+    //'capability_type'   => 'post',
+    //'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+    //'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+    'hierarchical' => false,
+    'supports' => array('title'), // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+    'taxonomies' => array(),
+    'has_archive' => false,
+    'rewrite' => true,
+    'query_var' => true,
+  ));
 }
 
 
